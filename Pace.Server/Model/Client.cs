0da855b7ec710238fb72ca0a1023de6f59ebcd5f
Bruno Camargo <bruno.ventzz@gmail.com>
2018-09-30 18:38:00 -0300

    Add Client model, viewmodel and bindings

---
+++ Pace.Server/Model/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pace.Server.Model
+{
+    public class Client
+    {
+        public string Identifier { get; set; }
+        public string Address { get; set; }
+        public int Port { get; set; }
+        public string Username { get; set; }
+        public string ComputerName { get; set; }
+        public string OS { get; set; }
+    }
+}
---
+++ Pace.Server/ViewModel/ClientViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Pace.Server.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pace.Server.ViewModel
+{
+    public class ClientViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<Client> Clients { get; set; }
+
+        public ClientViewModel()
+        {
+            Clients = new ObservableCollection<Client>()
+            {
+                new Client()
+                {
+                    Identifier = "PACE-01",
+                    Address = "127.0.0.1",
+                    Port = 5674,
+                    Username = "Bruno",
+                    ComputerName = "CORE",
+                    OS = "Windows 10 Home"
+                }
+            };
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
