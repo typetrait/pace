28fc53ba21329b0e9008b7e4fcf2ee86ca59c35b
Bruno Camargo <bruno.br.camargo@gmail.com>
2019-01-18 00:51:33 -0200

    Implement file size converter

---
+++ Pace.Server/Converters/FileSizeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Pace.Server.Converters
+{
+    [ValueConversion(typeof(long), typeof(string))]
+    public class FileSizeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            long size = (long)value;
+            return $"{size.ToString()} bytes";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value as string).Split(' ')[0];
+        }
+    }
+}
