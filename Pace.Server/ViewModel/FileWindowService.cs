3b6030b392c58737e870df990c8429e89fb3c5ab
Bruno Camargo <bruno.br.camargo@gmail.com>
2019-02-07 12:41:39 -0200

    Close File Explorer window on client disconnection

--- Pace.Server/ViewModel/FileWindowService.cs
+++ Pace.Server/ViewModel/FileWindowService.cs
@@ -1,6 +1,7 @@
 ﻿using Pace.Server.Model;
 using Pace.Server.Network;
 using Pace.Server.View;
+using System.Windows;
 
 namespace Pace.Server.ViewModel
 {
@@ -9,6 +10,19 @@ namespace Pace.Server.ViewModel
         public void ShowWindow(PaceServer server, ClientInfo client)
         {
             var window = new FileExplorerWindow(server, client);
+
+            // TODO: Probably not an optimal place to put this in, remember to handle this somewhere else
+            server.ClientDisconnected += (sender, args) =>
+            {
+                if (args.Client == client.Owner)
+                {
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        window.Close();
+                    });
+                }
+            };
+
             window.Show();
         }
     }
